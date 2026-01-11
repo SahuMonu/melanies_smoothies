af461cf3dc4c46440be88db753bc99731a3bed2f
SahuMonu <95745839+SahuMonu@users.noreply.github.com>
2026-01-11 05:45:21 +0000

    Use Our fruit_chosen Variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        st.subheader(fruit_choose+'Nutrition Information')
         # st.write(ingredients_string)
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     time_to_insert=st.button('Submit order')
