acf133f9763cc1f949c44ea71a79659b0141f34a
SahuMonu <95745839+SahuMonu@users.noreply.github.com>
2026-01-11 05:38:38 +0000

    API fruits list into selection list

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-   # st.write(ingredients_string)
+        # st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     time_to_insert=st.button('Submit order')
 
@@ -37,5 +39,4 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
