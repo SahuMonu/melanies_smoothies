746ae6ea2f5b688a65bbd871c4765dcb52ab915e
SahuMonu <95745839+SahuMonu@users.noreply.github.com>
2026-01-12 02:42:45 +0000

    changing name smoothiefroot_response to fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if ingredients_list:
         st.subheader(fruit_chosen+'Nutrition Information')
         # st.write(ingredients_string)
         fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
-        st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        st_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     time_to_insert=st.button('Submit order')
 
