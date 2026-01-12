39e728ca898ca0abab5832fb0c02b5f151683327
SahuMonu <95745839+SahuMonu@users.noreply.github.com>
2026-01-12 04:06:55 +0000

    using search_on in api

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingredients_list:
 
         st.subheader(fruit_chosen+' Nutrition Information')
         # st.write(ingredients_string)
-        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
-        st_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
+        st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     time_to_insert=st.button('Submit order')
 
